bdb525baa5329e0108a876911c7db0058d010757
Lia-1026 <139572097+Lia-1026@users.noreply.github.com>
2023-09-12 00:38:46 +0600

    Add files via upload

---
+++ Assignment No-2.c
@@ -0,0 +1,12 @@
+//Determine whether a number is abundant (the sum of its factors is greater than the number itself).
+#include<stdio.h>
+int main(){
+    int number ,sum=0;
+    scanf("%d",&number);
+    for(int i=1;i<number;i++){
+        if(number%i==0)sum+=i;
+    }
+    if(sum>number)printf("Abundant\n");
+    else printf("Not Abundant\n");
+return 0;
+}
